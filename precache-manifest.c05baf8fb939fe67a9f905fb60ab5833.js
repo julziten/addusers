bf2952f2ee5f58064679fa2bb03514d2aa483acb
Juliana Rocha <rochajulianacarolina@gmail.com>
2021-05-12 13:18:54 -0300

    Updates

---
+++ precache-manifest.c05baf8fb939fe67a9f905fb60ab5833.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d8cf7a80d931a9857fbe1c18006c3ecd",
+    "url": "/addusers/index.html"
+  },
+  {
+    "revision": "3724e63dea0730b4635a",
+    "url": "/addusers/static/css/main.72bb8b8d.chunk.css"
+  },
+  {
+    "revision": "b1f9f1d70b06a598da87",
+    "url": "/addusers/static/js/2.5f2c59f5.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/addusers/static/js/2.5f2c59f5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3724e63dea0730b4635a",
+    "url": "/addusers/static/js/main.6ee84474.chunk.js"
+  },
+  {
+    "revision": "12f82926db403a0fccfd",
+    "url": "/addusers/static/js/runtime-main.abefe3a0.js"
+  }
+]);
